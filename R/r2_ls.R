6a3691e01530eff6a921298359ece040d049ce19
daijiang <daijianglee@gmail.com>
2017-07-09 15:36:16 -0400

    update r2.ls by removing unused terms

--- R/r2_ls.R
+++ R/r2_ls.R
@@ -14,7 +14,7 @@ R2.ls <- function(mod = NULL, mod.r = NULL, phy = NULL) {
   }
 
   if (class(mod)[1] == "lmerMod") {
-    if (!exists(deparse(substitute(mod.r)))) {
+    if (!is.object(mod.r)) {
       # exists()?
       Y <- model.frame(mod)[, 1]
       mod.r <- lm(Y ~ 1)
@@ -26,257 +26,220 @@ R2.ls <- function(mod = NULL, mod.r = NULL, phy = NULL) {
   }
 
   if (class(mod)[1] == "glmerMod") {
-    if (family(mod)[[1]] != "binomial") {
-      stop("Sorry, but only binomial (binary) glmerMod models are allowed.")
+          if (!is.object(mod.r)) {
+              Y <- model.frame(mod)[, 1]
+              mod.r <- glm(Y ~ 1, family = family(mod)[[1]])
+          }
+          if (!is.element(class(mod.r)[1], c("glmerMod", "glm"))) {
+              stop("mod.r must be class glmerMod or glm.")
+          }
+          if (family(mod)[[1]] != family(mod.r)[[1]]) {
+              stop("Sorry, but mod and mod.r must be from the same family of distributions.")
+          }
+          return(R2.ls.glmerMod(mod, mod.r))
     }
-    if (!exists(deparse(substitute(mod.r)))) {
-      Y <- model.frame(mod)[, 1]
-      mod.r <- glm(Y ~ 1, family = family(mod)[[1]])
-    }
-    if (!is.element(class(mod.r)[1], c("glmerMod", "glm"))) {
-      stop("mod.r must be class glmerMod or glm.")
-    }
-    return(R2.ls.glmerMod(mod, mod.r)[1])
-  }
 
-  if (class(mod)[1] == "phylolm") {
-    if (!is.object(phy)) {
-      stop("For phylolm you must provide the phylo object")
+    if (class(mod)[1] == "phylolm") {
+        if (!is.object(phy)) {
+            stop("For phylolm you must provide the phylo object")
+        }
+        if (!is.object(mod.r)) {
+            Y <- mod$y
+            mod.r <- lm(Y ~ 1)
+        }
+        if (!is.element(class(mod.r)[1], c("phylolm", "lm"))) {
+            stop("mod.r must be class phylolm or lm.")
+        }
+        return(R2.ls.phylolm(mod, mod.r, phy))
     }
-    if (!exists(deparse(substitute(mod.r)))) {
-      Y <- mod$y
-      mod.r <- lm(Y ~ 1)
-    }
-    if (!is.element(class(mod.r)[1], c("phylolm", "lm"))) {
-      stop("mod.r must be class phylolm or lm.")
-    }
-    return(R2.ls.phylolm(mod, mod.r, phy))
-  }
 
-  if (class(mod)[1] == "binaryPGLMM") {
-    if (!exists(deparse(substitute(mod.r)))) {
-      Y <- mod$y
-      mod.r <- glm(Y ~ 1, family = "binomial")
+    if (class(mod)[1] == "binaryPGLMM") {
+        if (!is.object(mod.r)) {
+            Y <- mod$y
+            mod.r <- glm(Y ~ 1, family = "binomial")
+        }
+        if (!is.element(class(mod.r)[1], c("binaryPGLMM", "glm"))) {
+            stop("mod.r must be class binaryPGLMM or glm.")
+        }
+        return(R2.ls.binaryPGLMM(mod, mod.r))
     }
-    if (!is.element(class(mod.r)[1], c("binaryPGLMM", "glm"))) {
-      stop("mod.r must be class binaryPGLMM or glm.")
-    }
-    return(R2.ls.binaryPGLMM(mod, mod.r)[1])
-  }
 }
 
 R2.ls.lmerMod <- function(mod = NULL, mod.r = NULL) {
 
-  Y <- model.frame(mod)[,1]
-  X <- model.matrix(mod)
-  n <- dim(X)[1]
-  p <- dim(X)[2]
+    Y <- model.frame(mod)[, 1]
+    X <- model.matrix(mod)
+    n <- dim(X)[1]
+    p <- dim(X)[2]
 
-  X.r <- model.matrix(mod.r)
-  p.r <- dim(X.r)[2]
+    X.r <- model.matrix(mod.r)
+    p.r <- dim(X.r)[2]
 
-  D <- attr(mod,"pp")$LamtUt
-  V <- crossprod(D) + diag(n) # crossprod(D) is faster than t(D) %*% D
+    vcov <- as.data.frame(lme4::VarCorr(mod))$vcov
+    sigma2 <- vcov[length(vcov)]
 
-  if(class(mod.r) == "lmerMod"){
-    D.r <- attr(mod.r,"pp")$LamtUt
-    V.r <- crossprod(D.r) + diag(n)
-  }
+    if (class(mod.r) == "lmerMod") {
+        vcov.r <- as.data.frame(lme4::VarCorr(mod.r))$vcov
+        sigma2.r <- vcov.r[length(vcov.r)]
+    }
 
-  if(class(mod.r) == "lm"){
-    V.r <- diag(n)
-  }
+    if (class(mod.r) == "lm") {
+        sigma2.r <- (n - p.r)/n * stats::sigma(mod.r)^2
+    }
 
-  vcov <- as.data.frame(lme4::VarCorr(mod))$vcov
-
-  sigma2 <- vcov[length(vcov)]
-
-  if(class(mod.r) == "lmerMod"){
-    vcov.r <- as.data.frame(lme4::VarCorr(mod.r))$vcov
-    sigma2.r <- vcov.r[length(vcov.r)]
-  }
-
-  if(class(mod.r) == "lm"){
-    sigma2.r <- (n - p.r)/n * stats::sigma(mod.r)^2
-  }
-
-  R2.ls <- 1 - sigma2/sigma2.r
-
-  return(R2.ls)
+    R2.ls <- 1 - sigma2/sigma2.r
+    return(R2.ls)
 }
 
 R2.ls.glmerMod <- function(mod = NULL, mod.r = NULL) {
 
-  Y <- model.frame(mod)[, 1]
-  X <- model.matrix(mod)
-  n <- dim(X)[1]
-  p <- dim(X)[2]
+    Y <- model.frame(mod)[, 1]
+    X <- model.matrix(mod)
+    n <- dim(X)[1]
+    p <- dim(X)[2]
 
-  X.r <- model.matrix(mod.r)
-  p.r <- dim(X.r)[2]
+    X.r <- model.matrix(mod.r)
+    p.r <- dim(X.r)[2]
 
-  # full model
-  AVAg <- crossprod(attr(mod, "pp")$LamtUt)
-  Ag <- diag(1/attr(mod, "pp")$Xwts)
+    # full model
+    AVAg <- crossprod(attr(mod, "pp")$LamtUt)
+    Ag <- diag(1/attr(mod, "pp")$Xwts)
+    C <- Ag %*% AVAg %*% Ag
 
-  mu <- family(mod)$linkinv(X %*% lme4::fixef(mod))
+    mu <- family(mod)$linkinv(X %*% lme4::fixef(mod))
 
-  g <- mu * (1 - mu)
-  g <- g/prod(g)^(1/n)
-  V <- Ag %*% AVAg %*% Ag + diag(n)
-  iV <- solve(V)
+    if (family(mod)[1] == "binomial") v <- mu * (1 - mu)
+    if (family(mod)[1] == "poisson") v <- mu
 
-  iA <- diag(as.numeric(g^(-1/2)))
-  iAVA <- iA %*% iV %*% iA
-  SSE.ls <- t(Y - mu) %*% iAVA %*% (Y - mu)
+    sig2e <- pi^2/3
+    sig2a <- prod(diag(C))^(1/n)
+    # sig2a <- mean(diag(C))
+    Yhat <- log(mu/(1 - mu))
 
-  # reduced model
-  if (class(mod.r)[1] == "glmerMod") {
-    AVAg.r <- crossprod(attr(mod.r, "pp")$LamtUt)
-    Ag.r <- diag(1/attr(mod.r, "pp")$Xwts)
+    SSE.ls <- sig2e/(var(Yhat) + sig2a + sig2e)
 
-    mu.r <- family(mod.r)$linkinv(X.r %*% lme4::fixef(mod.r))
-    g.r <- mu.r * (1 - mu.r)
-    g.r <- g.r/prod(g.r)^(1/n)
-    V.r <- Ag.r %*% AVAg.r %*% Ag.r + diag(n)
-    iV.r <- solve(V.r)
+    # reduced model
+    if (class(mod.r)[1] == "glmerMod") {
+        AVAg.r <- crossprod(attr(mod.r, "pp")$LamtUt)
+        Ag.r <- diag(1/attr(mod.r, "pp")$Xwts)
 
-    iA.r <- diag(as.numeric(g.r^(-1/2)))
-    iAVA.r <- iA.r %*% iV.r %*% iA.r
-    SSE.ls.r <- t(Y - mu.r) %*% iAVA.r %*% (Y - mu.r)
-  }
+        C.r <- Ag.r %*% AVAg.r %*% Ag.r
 
-  if (class(mod.r)[1] == "glm") {
-    mu.r <- mod.r$fitted.values
-    g.r <- mu.r * (1 - mu.r)
-    g.r <- g.r/prod(g.r)^(1/n)
-    V.r <- diag(g.r)
-    iV.r <- diag(1/g.r)
-    SSE.ls.r <- t(Y - mu.r) %*% iV.r %*% (Y - mu.r)
-  }
+        mu.r <- family(mod.r)$linkinv(X.r %*% lme4::fixef(mod.r))
 
-  R2.ls <- 1 - SSE.ls/SSE.ls.r
+        if (family(mod.r)[1] == "binomial") v.r <- mu.r * (1 - mu.r)
+        if (family(mod.r)[1] == "poisson") v.r <- mu.r
 
-  return(R2.ls)
+        sig2e.r <- pi^2/3
+        sig2a.r <- prod(diag(C.r))^(1/n)
+        # sig2a.r <- mean(diag(C.r))
+        Yhat.r <- log(mu.r/(1 - mu.r))
+
+        SSE.ls.r <- sig2e.r/(var(Yhat.r) + sig2a.r + sig2e.r)
+    }
+
+    if (class(mod.r)[1] == "glm") {
+        mu.r <- mod.r$fitted.values
+
+        if (family(mod.r)[1] == "binomial") v.r <- mu.r * (1 - mu.r)
+        if (family(mod.r)[1] == "poisson") v.r <- mu.r
+
+        sig2e.r <- pi^2/3
+        Yhat.r <- log(mu.r/(1 - mu.r))
+
+        SSE.ls.r <- sig2e.r/(var(Yhat.r) + sig2e.r)
+    }
+
+    R2.ls <- 1 - SSE.ls/SSE.ls.r
+    return(R2.ls[1])
 }
 
 R2.ls.phylolm <- function(mod = NULL, mod.r = NULL, phy = NULL) {
 
-  Y <- mod$y
-  X <- mod$X
-  n <- dim(X)[1]
-  p <- dim(X)[2]
+    Y <- mod$y
+    X <- mod$X
+    n <- dim(X)[1]
+    p <- dim(X)[2]
 
-  optpar <- round(mod$optpar, digits = 4)
-
-  if (mod$model == "lambda") {
-    phy.f <- phylolm::transf.branch.lengths(phy,
-                                            parameters = list(lambda = optpar),
-                                            model = mod$model)$tree
-  }
-
-  if (mod$model %in% c("OUrandomRoot", "OUfixedRoot")) {
-    phy.f <- phylolm::transf.branch.lengths(phy,
-                                            parameters = list(alpha = optpar),
-                                            model = mod$model)$tree
-  }
-
-  V <- ape::vcv(phy.f)
-  scal <- sum(phy.f$edge.length)/n
-  V <- V/scal
-
-  sigma2 <- mod$sigma2
-
-  if (class(mod.r) == "phylolm") {
-    X.r <- mod.r$X
-    p.r <- dim(X.r)[2]
-
-    optpar.r <- round(mod.r$optpar, digits = 4)
-
+    optpar <- round(mod$optpar, digits = 4)
     if (mod$model == "lambda") {
-      phy.r <- phylolm::transf.branch.lengths(phy,
-                                              parameters = list(lambda = optpar.r),
-                                              model = mod.r$model)$tree
+      phy.f <- phylolm::transf.branch.lengths(phy,
+                                                parameters = list(lambda = optpar),
+                                                model = mod$model)$tree
     }
 
     if (mod$model %in% c("OUrandomRoot", "OUfixedRoot")) {
-      phy.r <- phylolm::transf.branch.lengths(phy,
-                                              parameters = list(alpha = optpar.r),
-                                              model = mod.r$model)$tree
+      phy.f <- phylolm::transf.branch.lengths(phy,
+                                              parameters = list(alpha = optpar),
+                                              model = mod$model)$tree
     }
 
-    V.r <- ape::vcv(phy.r)
-    scal.r <- sum(phy.r$edge.length)/n
-    V.r <- V.r/scal.r
-    sigma2.r <- mod.r$sigma2
-  }
+    scal <- sum(phy.f$edge.length)/n
+    sigma2 <- mod$sigma2
 
-  if (class(mod.r) == "lm") {
-    X.r <- model.matrix(mod.r)
-    p.r <- dim(X.r)[2]
-    V.r <- diag(n)
-    scal.r <- 1
-    sigma2.r <- (n - p.r)/n * sigma(mod.r)^2
-  }
+    if (class(mod.r) == "phylolm") {
+        X.r <- mod.r$X
+        p.r <- dim(X.r)[2]
 
-  R2.ls <- 1 - (scal * sigma2)/(scal.r * sigma2.r)
+        optpar.r <- round(mod.r$optpar, digits = 4)
 
-  return(R2.ls)
+        if (mod.r$model == "lambda") {
+          phy.r <- phylolm::transf.branch.lengths(phy,
+                                                  parameters = list(lambda = optpar.r),
+                                                  model = mod.r$model)$tree
+        }
+
+        if (mod.r$model %in% c("OUrandomRoot", "OUfixedRoot")) {
+          phy.r <- phylolm::transf.branch.lengths(phy,
+                                                  parameters = list(alpha = optpar.r),
+                                                  model = mod.r$model)$tree
+        }
+
+        scal.r <- sum(phy.r$edge.length)/n
+        sigma2.r <- mod.r$sigma2
+    }
+
+    if (class(mod.r) == "lm") {
+        X.r <- model.matrix(mod.r)
+        p.r <- dim(X.r)[2]
+        V.r <- diag(n)
+        scal.r <- 1
+        sigma2.r <- (n - p.r)/n * stats::sigma(mod.r)^2
+    }
+
+    R2.ls <- 1 - (scal * sigma2)/(scal.r * sigma2.r)
+    return(R2.ls)
 }
 
 R2.ls.binaryPGLMM <- function(mod = NULL, mod.r = NULL) {
-  mu <- mod$mu
-  n <- length(mu)
-  Yhat <- mu
-  R <- mod$y - mu
 
-  if (class(mod.r)[1] == "binaryPGLMM") {
-    mu.r <- mod.r$mu
-    Yhat.r <- mu.r
-    R.r <- mod.r$y - mu.r
-  } else {
-    mu.r <- mod.r$fitted.values
-    Yhat.r <- mu.r
-    R.r <- mod.r$y - mu.r
-  }
+    y <- mod$y
+    n <- length(y)
+    Yhat <- mod$X %*% mod$B
+    phyV <- mod$VCV
+    s2 <- mod$s2
 
-  phy <- mod$phy
+    sig2e <- pi^2/3
 
-  g <- mu * (1 - mu)
-  g <- g/prod(g)^(1/n)
-  V <- mod$s2 * ape::vcv(phy) + diag(n)
-  phy.temp <- ape::vcv2phylo(V)
-  V <- V/(sum(phy.temp$edge.length)/n)
-  iV <- solve(V)
-  iA <- diag(as.numeric(g^(-1/2)))
+    SSE.ls <- sig2e/(var(Yhat) + s2 + sig2e)
 
-  iAVA <- iA %*% iV %*% iA
-  SSE.ls <- t(R) %*% iAVA %*% R
+    # reduced model
+    if (class(mod.r)[1] == "binaryPGLMM") {
+        Yhat.r <- mod.r$X %*% mod.r$B
+        phyV.r <- mod.r$VCV
+        s2.r <- mod.r$s2
+        sig2e.r <- pi^2/3
+        SSE.ls.r <- sig2e.r/(var(Yhat.r) + s2.r + sig2e.r)
+    }
 
-  # reduced model
-  if (class(mod.r)[1] == "binaryPGLMM") {
-    g.r <- mu.r * (1 - mu.r)
-    g.r <- g.r/prod(g.r)^(1/n)
-    V.r <- mod.r$s2 * ape::vcv(phy) + diag(n)
-    phy.temp <- ape::vcv2phylo(V.r)
-    V.r <- V.r/(sum(phy.temp$edge.length)/n)
-    iV.r <- solve(V.r)
-    iA.r <- diag(as.numeric(g.r^(-1/2)))
+    if (class(mod.r)[1] == "glm") {
+        mu.r <- mod.r$fitted.values
+        sig2e.r <- pi^2/3
+        Yhat.r <- log(mu.r/(1 - mu.r))
+        SSE.ls.r <- sig2e.r/(var(Yhat.r) + sig2e.r)
+    }
 
-    iAVA.r <- iA.r %*% iV.r %*% iA.r
-    SSE.ls.r <- t(R.r) %*% iAVA.r %*% R.r
-  }
+    R2.ls <- 1 - SSE.ls/SSE.ls.r
 
-  if (class(mod.r)[1] == "glm") {
-    mu.r <- mod.r$fitted.values
-    g.r <- mu.r * (1 - mu.r)
-    g.r <- g.r/prod(g.r)^(1/n)
-    V.r <- diag(g.r)
-    iV.r <- diag(1/g.r)
-    SSE.ls.r <- t(R.r) %*% iV.r %*% R.r
-  }
-
-  R2.ls <- 1 - SSE.ls/SSE.ls.r
-
-  return(R2.ls)
+    return(R2.ls[1])
 }
