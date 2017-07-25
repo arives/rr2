d982a96eb9404c73017f41720978cede28f996bc
daijiang <daijianglee@gmail.com>
2017-07-25 17:28:56 -0400

    remove test about rr2::binaryPGLMM vs ape::binaryPGLMM

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -57,11 +57,12 @@ test_that("partial R2 should return values between 0 and 1", {
     expect_gt(partialR2(lm.x, lm.y), 0)
 })
 
-test_that("binaryPGLMM within rr2 should have same results as binaryPGLMM from ape", {
-    z.f1 <- ape::binaryPGLMM(y_phy_binary ~ x1, data = d, phy = phy)
-    z.x1 <- ape::binaryPGLMM(y_phy_binary ~ 1, data = d, phy = phy)
-    z.f <- rr2::binaryPGLMM(y_phy_binary ~ x1, data = d, phy = phy)
-    z.x <- rr2::binaryPGLMM(y_phy_binary ~ 1, data = d, phy = phy)
-    expect_identical(z.f1$s2, z.f$s2)
-    expect_identical(z.f1$B, z.f$B)
-})
+# there is a typo in ape::binaryPGLMM, which was corrected in rr2::binaryPGLMM, so not equal now...
+# test_that("binaryPGLMM within rr2 should have same results as binaryPGLMM from ape", {
+#     z.f1 <- ape::binaryPGLMM(y_phy_binary ~ x1, data = d, phy = phy)
+#     z.x1 <- ape::binaryPGLMM(y_phy_binary ~ 1, data = d, phy = phy)
+#     z.f <- rr2::binaryPGLMM(y_phy_binary ~ x1, data = d, phy = phy)
+#     z.x <- rr2::binaryPGLMM(y_phy_binary ~ 1, data = d, phy = phy)
+#     expect_identical(z.f1$s2, z.f$s2)
+#     expect_identical(z.f1$B, z.f$B)
+# })
