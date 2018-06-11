241187e03304fe4de21b850d8b40c6b9b7f70b4f
Daijiang Li <dli1@ufl.edu>
2018-06-11 16:10:27 -0400

    Update r2_lik.R

--- R/r2_lik.R
+++ R/r2_lik.R
@@ -7,9 +7,10 @@
 #' @export
 #'
 R2.lik <- function(mod = NULL, mod.r = NULL) {
+  if(class(mod)[1] == "merModLmerTest") class(mod) = "lmerMod"
   
-  if (!is.element(class(mod)[1], c("lm", "glm", "lmerMod", "merModLmerTest", "glmerMod", "phylolm", "phyloglm", "communityPGLMM"))) {
-    stop("mod must be class one of classes lm, glm, lmerMod (merModLmerTest), glmerMod, phylolm, phyloglm, communityPGLMM.")
+  if (!is.element(class(mod)[1], c("lm", "glm", "lmerMod", "glmerMod", "phylolm", "phyloglm", "communityPGLMM"))) {
+    stop("mod must be class one of classes lm, glm, lmerMod, glmerMod, phylolm, phyloglm, communityPGLMM.")
   }
   
   if (class(mod)[1] == "lm") {
@@ -33,8 +34,6 @@ R2.lik <- function(mod = NULL, mod.r = NULL) {
     }
     return(R2.lik.glm(mod, mod.r)[1])
   }
-  
-  if(class(mod)[1] == "merModLmerTest) class(mod) = "lmerMod"
   
   if (class(mod)[1] == "lmerMod") {
     if (!is.object(mod.r)) {
