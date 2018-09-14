b343cda65fd5fe8e6acbcf0d7f9dca6f703069de
arives <arives@users.noreply.github.com>
2018-09-14 11:34:22 -0500

    added copyright to R2

--- R/r2.R
+++ R/r2.R
@@ -1,8 +1,23 @@
 #' rr2: An R package to calculate R2s for regression models
 #' 
+# Copyright (C) 2018 Anthony Ives; Daijiang Li
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #' The rr2 package provides methods to calculate R2 for models with correlated errors, 
 #' including Phylogenetic GLS, Phylogenetic Logistic Regression, LMMs, GLMM, and PGLMM.
-#' 
+#'  
 #' @docType package
 #' @name rr2
 NULL
